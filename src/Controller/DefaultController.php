bb40423edc0da74c1840412bb2c39ab4c2933c02
Hicham Benzaied <hichambenzaied69150@gmail.com>
2023-06-14 16:14:44 +0200

    Liste_5_dernieres_annonces

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AnnonceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_default')]
-    public function index(): Response
+    public function Index(AnnonceRepository $annonceRepository): Response
     {
+        $annonces = $annonceRepository->annonceList();
+
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'annonces' => $annonces, 
         ]);
     }
 }
