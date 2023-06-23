1432f3c4075841d5d1d828deb7c3ccf87a54d435
Hicham Benzaied <hichambenzaied69150@gmail.com>
2023-06-23 09:25:08 +0200

    Mon_Compte

--- src/Controller/AdoptantController.php
+++ src/Controller/AdoptantController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class AdoptantController extends AbstractController
 {
     #[Route('/adoptant', name: 'app_adoptant')]
+    #[IsGranted('ROLE_ADOPTANT')]
     public function index(): Response
     {
         return $this->render('adoptant/index.html.twig', [
