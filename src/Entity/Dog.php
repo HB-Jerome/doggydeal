27ccb03b5f82953dc8b9111a5d76b6c5be69b9d7
Snow <koko@gmail.com>
2023-06-27 16:26:02 +0200

    Rajuste API sur Liste des annonce Détails d'une annonce Liste des chiens Détails des chiens Liste des races avec les chiens associés

--- src/Entity/Dog.php
+++ src/Entity/Dog.php
@@ -2,6 +2,9 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use App\Repository\DogRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -9,6 +12,12 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: DogRepository::class)]
+
+#[ApiResource(
+    operations: [
+        new Get(),
+    ],
+)]
 class Dog
 {
     #[ORM\Id]
