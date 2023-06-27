cef551e84b658132a921559e3c23e9e3afc16add
Jérôme Forest <jforest@stagiaire-humanbooster.com>
2023-06-27 14:40:23 +0200

    seul annonceur peut accéder à sa page

--- src/Controller/AnnonceurController.php
+++ src/Controller/AnnonceurController.php
@@ -6,10 +6,24 @@ use App\Repository\AnnonceurRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+// class AnnonceurController extends AbstractController
+// {
+//     #[Route('/annonceur', name: 'app_annonceur')]
+//     public function index(AnnonceurRepository $annonceurRepository): Response
+//     {
+//         $annonces=$annonceurRepository->annonceListSPA();
+//         return $this->render('annonceur/index.html.twig', [
+//             'controller_name' => 'AnnonceurController',
+//             'annonces' => $annonces
+//         ]);
+//     }
+// }
 class AnnonceurController extends AbstractController
 {
     #[Route('/annonceur', name: 'app_annonceur')]
+    #[IsGranted('ROLE_ANNONCEUR')]
     public function index(AnnonceurRepository $annonceurRepository): Response
     {
         $annonces=$annonceurRepository->annonceListSPA();
