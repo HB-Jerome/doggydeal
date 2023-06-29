9f951ce252b9f520090d1be430f229afda7ef548
Jérôme Forest <jforest@stagiaire-humanbooster.com>
2023-06-29 15:28:38 +0200

    création  formulaires : DogType, ImageType

--- src/Form/CreationAnnonceType.php
+++ src/Form/CreationAnnonceType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Annonce;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionTypeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,6 +15,17 @@ class CreationAnnonceType extends AbstractType
     {
         $builder
             ->add('title',TextType::class,['label'=>'title','required'=>true])
+            ->add(
+                'dog', CollectionType::class,
+                [
+                    'entry_type' => DogType::class,
+                    'label' => 'dogs',
+                    'prototype_name' => 'dogs',
+                    'allow_add' => true,
+                    'by_reference' => false,
+                    'entry_options' => ['label' => false],
+                ]
+            )
             ->add('isAvailable')
         ;
     }
